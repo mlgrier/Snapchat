95ad53e2027c05f3061290cab258e064977ccbbc
Unknown <mlgrier@gmail.com>
2017-09-02 17:49:31 -0500

    added the main page

--- Snapchat/ViewController.swift
+++ Snapchat/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
 }
 
