f894df7bf7e79da88a8f368e684bda2b54e5629f
Unknown <mlgrier@gmail.com>
2017-09-04 19:33:59 -0500

    image viewController added

---
+++ Snapchat/AddImageViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddImageViewController.swift
+//  Snapchat
+//
+//  Created by Marco Grier on 9/4/17.
+//  Copyright © 2017 Marco Grier. All rights reserved.
+//
+
+import UIKit
+
+class AddImageViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var descriptionText: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func folderTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func nextTapped(_ sender: Any) {
+    }
+    
+}
