3910891d9433ba7f79c358eb425e42fa4b866802
Unknown <mlgrier@gmail.com>
2017-09-05 19:10:36 -0500

    updated code to upload pics to firebase

--- Podfile
+++ Podfile
@@ -8,5 +8,6 @@ target 'Snapchat' do
   # Pods for Snapchat
   pod 'Firebase/Core'
   pod 'Firebase/Auth'
+  pod 'Firebase/Storage'
 
 end
--- Snapchat/AddImageViewController.swift
+++ Snapchat/AddImageViewController.swift
@@ -7,25 +7,57 @@
 //
 
 import UIKit
+import FirebaseStorage
 
-class AddImageViewController: UIViewController {
+class AddImageViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var descriptionText: UITextField!
+    
+    var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
 
     @IBAction func folderTapped(_ sender: Any) {
+        //Access to the users photo library - up date privacy in info.plist
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+        
     }
     
     @IBAction func cameraTapped(_ sender: Any) {
+        //Access to the users camera - up date privacy in info.plist
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let selectedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = selectedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func nextTapped(_ sender: Any) {
+        
+        let imageFolder = Storage.storage().reference().child("images")
+        
+        if let image = imageView.image {
+            if let imageData = UIImageJPEGRepresentation(image, 0.1) {
+                imageFolder.child("myPick.jpeg").putData(imageData, metadata: nil, completion: { (metadata, error) in
+                    if let error = error {
+                        print(error)
+                    } else {
+                        print("Upload Complete")
+                    }
+                })
+            }
+        }
+        
     }
     
 }
